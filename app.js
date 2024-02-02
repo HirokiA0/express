f9551a179a1ba38730253b169ca05e54c4559cd4
unknown <asahi1001bd@gmail.com>
2024-02-03 00:38:54 +0900

    a

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 var qrRouter = require('./routes/qr');
 var dogRouter = require('./routes/dog');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/notes',notesRouter);
 app.use('/cat',catRouter);
 app.use('/qr',qrRouter);
 app.use('/dog',dogRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
